2714edd1db701183a193b4d61ee5e55a0f2fb572
Rasmus Tanggaard <rasmus@tthost.dk>
2023-11-15 11:33:47 +0100

    Database integration added

---
+++ Domain Models/Database/DatabaseHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models.Database
+{
+    public class DatabaseHandler
+    {
+        public IDatabaseEntry GetDBEntry(int pKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDatabaseEntry[] GetDatabaseEntries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TryAddDBEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TryRemoveDBEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryUpdateDBEntry()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
