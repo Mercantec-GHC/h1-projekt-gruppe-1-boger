74afbd6a584d931b522b3f4816353877fc411ba8
Yevheniia Zhygalova <119417215+YevaIT@users.noreply.github.com>
2023-11-15 12:45:39 +0100

    Add main product related classes

---
+++ Domain Models/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models
+{
+   public class Media
+    {
+        public int Id { get; set; }
+        public string? Titel { get; set; }
+      
+        // public Language Language { get; set; } ||waiting for enum||
+        public List<Tag>? Tags { get; set; }
+    }
+}
---
+++ Domain Models/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain_Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }  
+        public string? Name { get; set; }
+    }
+}
