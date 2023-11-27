54c103f324567b13fc7736adabc792445f4cfec5
Yevheniia Zhygalova <119417215+YevaIT@users.noreply.github.com>
2023-11-27 10:04:58 +0100

    fixed age and players

--- Domain Models/BoardGame.cs
+++ Domain Models/BoardGame.cs
@@ -9,8 +9,9 @@ namespace Domain_Models
     public class BoardGame:Media
     {
         public string? Developer { get; set; }
-        public string? PlayerCount { get; set; }
-        public string? Age { get; set; }
+        public int MinPlayerCount { get; set; }
+        public int MaxPlayerCount { get; set; }
+        public int MinAge { get; set; }
         public int PlayTime { get; set; }
 
     }
