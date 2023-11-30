ea9aab372b68e6b22c800cca267a414dc76a1f04
Rasmus Tanggaard <rasmus@tthost.dk>
2023-11-30 14:50:49 +0100

    Add database connection stuff

--- Domain Models/Database/DatabaseHandler.cs
+++ Domain Models/Database/DatabaseHandler.cs
@@ -3,32 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Domain_Models.Database
 {
-    public class DatabaseHandler
+    public static class DatabaseHandler
     {
-        public IDatabaseEntry GetDBEntry(int pKey)
+        private static SqlConnection _connection;
+        private static bool _isConnected = false;
+
+        public static bool IsConnected { get => _isConnected; set => _isConnected = value; }
+
+        public static bool Create(string? connectionstring = "none")
+        {
+            if (connectionstring == "none")
+                connectionstring = Environment.GetEnvironmentVariable("connectionstring");
+
+            try
+            {
+                _connection = new SqlConnection(connectionstring);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool Connect()
+        {
+            if (_connection == null)
+                Create();
+
+            try
+            {
+                _connection.Open();
+                IsConnected = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool Disconnect()
+        {
+            try
+            {
+                _connection.Close();
+                IsConnected = false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string GetDBEntry(int pKey, string tablename)
         {
             throw new NotImplementedException();
         }
 
-        public IDatabaseEntry[] GetDatabaseEntries()
+        public static string GetDatabaseEntries()
         {
             throw new NotImplementedException();
         }
 
-        public void TryAddDBEntry()
+        public static void TryAddDBEntry()
         {
             throw new NotImplementedException();
         }
 
-        public void TryRemoveDBEntry()
+        public static void TryRemoveDBEntry()
         {
             throw new NotImplementedException();
         }
 
-        public bool TryUpdateDBEntry()
+        public static bool TryUpdateDBEntry()
         {
             throw new NotImplementedException();
         }
