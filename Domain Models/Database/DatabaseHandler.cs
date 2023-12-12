6d58906ad58fbe6c45ffc95dc67375deacc701f2
Yevheniia Zhygalova <119417215+YevaIT@users.noreply.github.com>
2023-12-12 15:00:02 +0100

    made DBmethods for User

--- Domain Models/Database/DatabaseHandler.cs
+++ Domain Models/Database/DatabaseHandler.cs
@@ -7,6 +7,7 @@ namespace Domain_Models.Database
         private static SqlConnection _connection;
         private static bool _isConnected = false;
 
+        public static string FieldDelimiter = "\n";
         public static bool IsConnected { get => _isConnected; set => _isConnected = value; }
 
         public static bool Create(string? connectionstring = "Server=tcp:secondhandbooks.database.windows.net,1433;Initial Catalog=SecondhandBooksDB;Persist Security Info=False;User ID=secbookadmin;Password=Prideandprejudice!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
@@ -108,7 +109,7 @@ namespace Domain_Models.Database
                 {
                     response += reader.GetValue(i) + " ";
                 }
-            }   response += "\n";
+            }   response += FieldDelimiter;
 
             return response;
         }
