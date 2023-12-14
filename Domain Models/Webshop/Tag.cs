0e494ec95af89f59b8a4cc4857352ef5c9e32791
Rasmus Tanggaard <rasmus@tthost.dk>
2023-12-14 01:34:39 +0100

    Implemented tag databaseentry methods

--- Domain Models/Webshop/Tag.cs
+++ Domain Models/Webshop/Tag.cs
@@ -1,4 +1,5 @@
 ﻿using Domain_Models.Database;
+using Microsoft.Data.SqlClient;
 
 namespace Domain_Models.Webshop
 {
@@ -9,22 +10,54 @@ namespace Domain_Models.Webshop
 
         public void AddDBEntry()
         {
-            throw new NotImplementedException();
+            SqlCommand cmd = new SqlCommand("INSERT INTO tag_table (tag) OUTPUT Inserted.tag_id VALUES (@name)");
+            cmd.Parameters.AddWithValue("@name", Name);
+
+            string[] result = DatabaseHandler.FetchFromTable(cmd).Split(DatabaseHandler.FieldDelimiter);
+
+            Id = int.Parse(result[0]);
+
         }
 
         public IDatabaseEntry GetDBEntry(int pKey)
         {
-            throw new NotImplementedException();
+            if (pKey == 0)
+                return null;
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tag_table WHERE tag_id = @id");
+            cmd.Parameters.AddWithValue("@id", pKey);
+
+            string[] result = DatabaseHandler.FetchFromTable(cmd).Split(DatabaseHandler.FieldDelimiter);
+
+            Id = int.Parse(result[0]);
+            Name = result[1];
+
+            return this;
         }
 
         public void RemoveDBEntry()
         {
-            throw new NotImplementedException();
+            if (Id == 0)
+                return;
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM tag_table WHERE tag_id = @id");
+            cmd.Parameters.AddWithValue("@id", Id);
+
+            DatabaseHandler.FetchFromTable(cmd);
         }
 
         public bool UpdateDBEntry()
         {
-            throw new NotImplementedException();
+            if (Id == 0)
+                return false;
+
+            SqlCommand cmd = new SqlCommand("UPDATE tag_table SET tag = @name WHERE tag_id = @id");
+            cmd.Parameters.AddWithValue("@id", Id);
+            cmd.Parameters.AddWithValue("@name", Name);
+
+            DatabaseHandler.FetchFromTable(cmd);
+
+            return true;
         }
     }
 }
