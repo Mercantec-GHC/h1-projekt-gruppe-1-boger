6a3cbebf7701f8c193d5d69f437112f6f2c0e484
Rasmus Tanggaard <rasmus@tthost.dk>
2023-12-19 12:25:17 +0100

    Safe features

--- Domain Models/Database/DatabaseHandler.cs
+++ Domain Models/Database/DatabaseHandler.cs
@@ -14,7 +14,7 @@ namespace Domain_Models.Database
         public static string RowDelimiter = "\n";
         public static bool IsConnected { get => _isConnected; set => _isConnected = value; }
 
-        public static bool Create(string? connectionstring = "Server=tcp:secondhandbooks.database.windows.net,1433;Initial Catalog=SecondhandBooksDB;Persist Security Info=False;User ID=secbookadmin;Password=Prideandprejudice!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        public static bool Create(string? connectionstring = "none")
         {
             if (connectionstring == "none")
                 connectionstring = Environment.GetEnvironmentVariable("connectionstring");
@@ -135,7 +135,7 @@ namespace Domain_Models.Database
         public static Listing[] Search(Filter filter)
         {
             // TODO: Implement search
-            string results = FetchFromTable(new SqlCommand(filter.GetSqlCommand()));
+            string results = FetchFromTable(filter.GetSqlCommand());
 
 
             if (results == "EMPTY")
